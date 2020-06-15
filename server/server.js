2eb9653509704b548625599f76d2aa3b1e05347e
Tri Vuong <trivektor@gmail.com>
2020-06-14 21:17:02 -0700

    Fixed issue where routing didn't work

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
