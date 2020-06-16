55cd463a701600b1ed2858eac0d5392fbc3558cb
Tri Vuong <trivektor@gmail.com>
2020-06-16 01:06:30 -0700

    Switch to MongoDB Atlas

--- server/dbconnection.js
+++ server/dbconnection.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = require('bluebird');
 
-module.exports = mongoose.connect(process.env.CHATTYY_MLAB_CONNECTION_STRING, {
+module.exports = mongoose.connect(
+  process.env.CHATTYY_DB_CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
