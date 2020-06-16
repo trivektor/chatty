2460e0795c4e2e09d28a0c95a2bc3faa0ad55583
Tri Vuong <trivektor@gmail.com>
2020-06-16 00:18:00 -0700

    Only run this in production

--- server/server.js
+++ server/server.js
@@ -27,10 +27,13 @@ apiRouter.post('/rooms', async (req, res) => {
 });
 
 app.use('/api', apiRouter);
-app.use(express.static(path.join(__dirname, '../build')));
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../build')));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  });
+}
 
 io
   .of('/chatty')
